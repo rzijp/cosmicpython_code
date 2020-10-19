299c79bd5b07cff99cda7b0ba2a6ef358922dd27
Robbert Zijp <git@rzijp.dds.nl>
2020-10-19 20:08:30 +0000

    first tests working

---
+++ model.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+
+
+@dataclass()
+class Orderline:
+    sku: str
+    quantity: int
+
+
+@dataclass()
+class Batch:
+    id: str
+    sku: str
+    quantity: int
+
+    def allocate(self, orderline: Orderline):
+        if orderline.quantity <= self.quantity:
+            self.quantity = self.quantity - orderline.quantity
+            return True
+        else:
+            return False
--- test_model.py
+++ test_model.py
@@ -1,27 +1,45 @@
 from datetime import date, timedelta
+
 import pytest
 
-# from model import ...
+from model import Batch, Orderline
 
 today = date.today()
 tomorrow = today + timedelta(days=1)
 later = tomorrow + timedelta(days=10)
 
-def test_allocating_to_a_batch_reduces_the_available_quantity():
-    pytest.fail('todo')
 
 def test_can_allocate_if_available_greater_than_required():
-    pytest.fail('todo')
+    batch = Batch(id="123", sku="RED CHAIR", quantity=10)
+    orderline = Orderline(sku="RED CHAIR", quantity=2)
+    assert batch.allocate(orderline)
+
+
+def test_allocating_to_a_batch_reduces_the_available_quantity():
+    batch = Batch(id="123", sku="RED CHAIR", quantity=10)
+    orderline = Orderline(sku="RED CHAIR", quantity=2)
+    batch.allocate(orderline)
+    assert batch.quantity == 8
+
 
 def test_cannot_allocate_if_available_smaller_than_required():
-    pytest.fail('todo')
+    batch = Batch(id="123", sku="RED CHAIR", quantity=10)
+    orderline = Orderline(sku="RED CHAIR", quantity=20)
+    assert not (batch.allocate(orderline))
+
 
 def test_can_allocate_if_available_equal_to_required():
-    pytest.fail('todo')
+    batch = Batch(id="123", sku="RED CHAIR", quantity=10)
+    orderline = Orderline(sku="RED CHAIR", quantity=10)
+    batch.allocate(orderline)
+    assert batch.quantity == 0
 
+
+@pytest.mark.xfail()
 def test_prefers_warehouse_batches_to_shipments():
-    pytest.fail('todo')
+    pytest.fail("todo")
 
+
+@pytest.mark.xfail()
 def test_prefers_earlier_batches():
-    pytest.fail('todo')
-
+    pytest.fail("todo")
